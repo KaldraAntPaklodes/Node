3f0b921733c5078d957208934a64947aeac016d3
KaldraAntPaklodes <bliudziust@gmail.com>
2023-04-13 19:27:45 +0300

    Add new node lesson project

---
+++ 1_Paskaita/index.js
@@ -0,0 +1,10 @@
+console.log(`Mano Vardas Tautvydas`)
+
+const casual = require(`casual`)
+console.log(casual.city)
+
+console.log(casual.integer((from = 1), (to = 10)))
+
+const name = `${casual.name_prefix} ${casual.first_name} ${casual.last_name}`
+
+console.log(name)
